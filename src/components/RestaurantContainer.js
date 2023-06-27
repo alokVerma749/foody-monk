258a727ba326da014944d032e3190dff4432772c
alok verma <alokverma749@gmail.com>
2023-06-27 12:11:54 +0530

    added search feature

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -8,12 +8,14 @@ import Shimmer from "./Shimeer";
 
 const ResaturantContainer = () => {
     const [listOfRestaurants, setlistOfRestaurants] = useState([]);
+    const [listOfFilteredRestaurants, setListOfFilteredRestaurants] = useState([]);
     const [searchText, setSearchText] = useState("");
 
     const fetchData = async () => {
         const data = await fetch(RESTAURANT_API);
         const json = await data.json();
         setlistOfRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+        setListOfFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
     }
 
     useEffect(() => {
@@ -23,25 +25,39 @@ const ResaturantContainer = () => {
     return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <>
             <div className="serch-box flex flex-row justify-between p-5 w-full">
+
+                {/* Search field */}
                 <div className="search">
-                    <input type="text" name="search" id="search" placeholder='SEARCH' className='outline-none shadow-md shadow-orange-800 p-2' />
-                    <button type="submit" className='bg-lime-400 p-1 mx-4 rounded-md text-white'>Search</button>
+                    <input value={searchText} onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }}
+                        type="text" name="search" id="search" placeholder='SEARCH' className='outline-none shadow-md shadow-orange-800 p-2' />
+                    <button onClick={() => {
+                        console.log(searchText);
+                        const searchRestaurant = listOfRestaurants.filter(restaurant => (
+                            restaurant.data.name.toLowerCase().includes(searchText.toLowerCase())
+                        ))
+                        setListOfFilteredRestaurants(searchRestaurant)
+
+                    }} type="submit" className='bg-lime-400 p-1 mx-4 rounded-md text-white'>Search</button>
                 </div>
+
+                {/* filter button */}
                 <button type="button" className='bg-lime-400 p-2 flex rounded-md text-white w-fit'
                     onClick={() => {
-                        const filteredList = listOfRestaurants.filter(
+                        const filteredList = listOfFilteredRestaurants.filter(
                             (restaurant) => restaurant.data.avgRating >= 4
                         )
-                        setlistOfRestaurants(filteredList);
-                        console.log(listOfRestaurants);
+                        setListOfFilteredRestaurants(filteredList);
                     }}>
                     Filter To Rated Restaurants
                 </button>
-            </div>
+            </div >
 
+            {/* cards container */}
             <div className="res-container flex flex-row flex-wrap">
                 {
-                    listOfRestaurants.map((card) => {
+                    listOfFilteredRestaurants.map((card) => {
                         return (
                             < RestaurantCard key={card.data.id} resData={card} />
                         )
