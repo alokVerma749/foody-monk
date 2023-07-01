527e59f09349ee01c54d2d2481efa22a0ccedd4f
alok verma <alokverma749@gmail.com>
2023-07-01 20:06:21 +0530

    improved ux in cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,20 @@
 import { useSelector } from "react-redux";
 import { MENU_IMG_URL } from "../../utils/constants";
 
+import { useDispatch } from "react-redux";
+import { clearCart } from "../../utils/cartSlice";
+import { Link } from "react-router-dom";
+
 const Cart = () => {
-    const cartItems = useSelector(store => store.cart.items)
+    const cartItems = useSelector(store => store.cart.items);
+    const dispatch = useDispatch()
+    const handleClearCart = () => {
+        dispatch(clearCart());
+    }
     return (
         <div className="flex flex-col mx-auto max-w-3xl p-6 space-y-4 sm:p-10 bg-gray-50 text-gray-800">
             <h2 className="text-xl font-semibold">Your cart</h2>
+            <button className="bg-red-600 text-white w-fit p-1 rounded-md " onClick={handleClearCart}>Clear Cart</button>
             <ul className="flex flex-col divide-y divide-gray-300">
                 {
                     cartItems.map(cartItem => (
@@ -54,11 +63,11 @@ const Cart = () => {
                 <p className="text-sm text-gray-600">Not including taxes and shipping costs</p>
             </div>
             <div className="flex justify-end space-x-4">
-                <button type="button" className="px-6 py-2 border rounded-md border-indigo-600">Back
-                    <span className="sr-only sm:not-sr-only">to shop</span>
-                </button>
+                <Link to={"/"} type="button" className="px-6 py-2 border rounded-md border-indigo-600">Back
+                    <span className="sr-only sm:not-sr-only"> to shop</span>
+                </Link>
                 <button type="button" className="px-6 py-2 border rounded-md bg-indigo-600 text-gray-50 border-indigo-600">
-                    <span className="sr-only sm:not-sr-only">Continue to</span>Checkout
+                    <span className="sr-only sm:not-sr-only">Continue to</span> Checkout
                 </button>
             </div>
         </div>
