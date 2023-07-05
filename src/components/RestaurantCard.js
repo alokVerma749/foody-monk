25344adf6ae89e0cc4dde2185f4e8c2bf6f280fc
Jalandhar Paswan <mrjalandharpaswan143@gmail.com>
2023-07-05 18:36:05 +0530

    Improved RestaurantCard.js

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,16 +2,24 @@ import { IMG_URL } from "../../utils/constants";
 
 const RestaurantCard = (props) => {
     const { name, cloudinaryImageId, cuisines, area, costForTwo, avgRating } = props.resData.data;
+
     return (
-        <div className="h-fit flex flex-col gap-3">
-            <img src={IMG_URL + cloudinaryImageId} alt="res-img" className="rounded-md" />
-            <p className="name"><span className="font-semibold">Name:</span> {name}</p>
-            <p className="cuisines"><span className="font-semibold">Cuisines:</span> {cuisines.join(", ")}</p>
-            <p className="area"><span className="font-semibold">Address:</span> {area}</p>
-            <p className="costForTwo"><span className="font-semibold">Rs.</span> {costForTwo / 100}</p>
-            <p className="ratings"><span className="font-semibold">Ratings:</span> {avgRating}</p>
+        <div className="h-fit rounded overflow-hidden bg-white">
+            <img className="w-fit object-cover object-center" src={IMG_URL + cloudinaryImageId} alt="Food Image" />
+            <div className="px-6 pt-4">
+                <div className="font-bold text-xl mb-2">{name}</div>
+                <p className="text-gray-700 text-sm mb-2">Cuisines: {cuisines.join(", ")}</p>
+                <p className="text-gray-700 text-sm mb-2">Address: {area}</p>
+                <p className="text-gray-700 text-sm mb-2">Rs. {costForTwo / 100}</p>
+                <div className="inline-flex items-center bg-green-600 rounded p-1">
+                    <svg className="w-4 h-4 fill-current text-white mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M10 1L12.39 6.36L18.18 7.27L13.96 11.18L15.09 16.14L10 13.77L4.91 16.14L6.04 11.18L1.82 7.27L7.61 6.36L10 1Z" />
+                    </svg>
+                    <p className="text-white text-sm font-bold">{avgRating}</p>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default RestaurantCard;
