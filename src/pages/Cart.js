abb371d0c1268f0ab09bed027029edbd0c6c197f
Jalandhar Paswan <mrjalandharpaswan143@gmail.com>
2023-07-07 13:11:31 +0530

    Bug fixed!

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,8 +2,7 @@ import { useSelector } from "react-redux";
 import { MENU_IMG_URL } from "../../utils/constants";
 
 import { useDispatch } from "react-redux";
-import { clearCart } from "../../utils/cartSlice";
-import { removeItem } from "../../utils/cartSlice";
+import { clearCart, removeItem } from "../../utils/cartSlice";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
@@ -58,8 +57,8 @@ const Cart = () => {
                                         </div>
                                     </div>
                                     <div className="flex text-sm">
-                                        <button type="button" className="flex items-center px-2 py-1 pl-0 space-x-1" onClick={() => handleRemoveItem(cartItem.card.info.id)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
+                                        <button type="button" className="hover:text-red-700 flex items-center px-2 py-1 pl-0 space-x-1" onClick={() => handleRemoveItem(cartItem.card.info.id)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-red-700">
                                                 <path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
                                                 <rect width="32" height="200" x="168" y="216"></rect>
                                                 <rect width="32" height="200" x="240" y="216"></rect>
@@ -67,12 +66,6 @@ const Cart = () => {
                                                 <path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
                                             </svg>
                                             <span>Remove</span>
-                                        </button>
-                                        <button type="button" className="flex items-center px-2 py-1 space-x-1">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-                                                <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-                                            </svg>
-                                            <span>Add to favorites</span>
                                         </button>
                                     </div>
                                 </div>
