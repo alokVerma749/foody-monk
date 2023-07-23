4ef165fe3a5e0101728c6b8ccf3b80e3220603c9
alok verma <alokverma749@gmail.com>
2023-07-23 08:02:22 +0530

    change color of rating BG

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -12,7 +12,7 @@ const RestaurantCard = (props) => {
                 <p className="text-gray-700 text-sm mb-2">Address: {area}</p>
                 <p className="text-gray-700 text-sm mb-2">Rs. {costForTwo / 100}</p>
                 {
-                    <div className={`${(avgRating > 3.9 ? "bg-green-600" : "bg-red-600")} inline-flex items-center rounded p-1`}>
+                    <div className={`${(avgRating > 3.9 ? "bg-green-600" : "bg-orange-500-600")} inline-flex items-center rounded p-1`}>
                         <svg className="w-4 h-4 fill-current text-white mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path d="M10 1L12.39 6.36L18.18 7.27L13.96 11.18L15.09 16.14L10 13.77L4.91 16.14L6.04 11.18L1.82 7.27L7.61 6.36L10 1Z" />
                         </svg>
