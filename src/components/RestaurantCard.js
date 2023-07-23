6f6e84a38cf717814a6dd18972407acea02b187d
alok verma <alokverma749@gmail.com>
2023-07-23 07:58:00 +0530

    added dynamic BG to rating box

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -11,12 +11,14 @@ const RestaurantCard = (props) => {
                 <p className="text-gray-700 text-sm mb-2">Cuisines: {cuisines.join(", ")}</p>
                 <p className="text-gray-700 text-sm mb-2">Address: {area}</p>
                 <p className="text-gray-700 text-sm mb-2">Rs. {costForTwo / 100}</p>
-                <div className="inline-flex items-center bg-green-600 rounded p-1">
-                    <svg className="w-4 h-4 fill-current text-white mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M10 1L12.39 6.36L18.18 7.27L13.96 11.18L15.09 16.14L10 13.77L4.91 16.14L6.04 11.18L1.82 7.27L7.61 6.36L10 1Z" />
-                    </svg>
-                    <p className="text-white text-sm font-bold">{avgRating}</p>
-                </div>
+                {
+                    <div className={`${(avgRating > 3.9 ? "bg-green-600" : "bg-red-600")} inline-flex items-center rounded p-1`}>
+                        <svg className="w-4 h-4 fill-current text-white mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M10 1L12.39 6.36L18.18 7.27L13.96 11.18L15.09 16.14L10 13.77L4.91 16.14L6.04 11.18L1.82 7.27L7.61 6.36L10 1Z" />
+                        </svg>
+                        <p className="text-white text-sm font-bold">{avgRating}</p>
+                    </div>
+                }
             </div>
         </div>
     );
