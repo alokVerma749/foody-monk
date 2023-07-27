ef14081a27a5ea3151863fd53855898e56cf29e1
alok verma <alokverma749@gmail.com>
2023-07-27 22:23:12 +0530

    added message send status

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,8 +12,10 @@ const Contact = () => {
     subject: "",
     message: ""
   })
+  const [isSending, setIsSending] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsSending(true);
     sendMessage();
     setInfo({
       email: "",
@@ -25,6 +27,7 @@ const Contact = () => {
     try {
       const response = await axios.post(URL + 'user/contact', info)
       if (response.data.success) {
+        setIsSending(false);
         toast.success(response.data.message, {
           position: "top-right",
           autoClose: 5000,
@@ -36,6 +39,7 @@ const Contact = () => {
           theme: "light",
         });
       } else {
+        setIsSending(false);
         toast.error(response.message, {
           position: "top-right",
           autoClose: 5000,
@@ -48,6 +52,7 @@ const Contact = () => {
         });
       }
     } catch (error) {
+      setIsSending(false);
       toast.error(error.message, {
         position: "top-right",
         autoClose: 5000,
@@ -99,7 +104,7 @@ const Contact = () => {
               message: e.target.value
             })} id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..."></textarea>
           </div>
-          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
+          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300">{isSending ? <span>Sending..</span> : <span>Send Message</span>}</button>
         </form>
       </div>
     </section>
